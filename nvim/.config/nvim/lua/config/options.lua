52e757f18ad808262ee02729b7a13492fd6e73f8
John Carlo Manuel <39042556+johncmanuel@users.noreply.github.com>
2025-02-01 19:36:55 -0800

    update options

--- nvim/.config/nvim/lua/config/options.lua
+++ nvim/.config/nvim/lua/config/options.lua
@@ -2,8 +2,7 @@
 -- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
 -- Add any additional options here
 
-local opt = vim.opt
-opt.clipboard = "unnamedplus"
+vim.opt.clipboard = "unnamedplus"
 
 -- Enable the option to require a Prettier config file
 -- If no prettier config file is found, the formatter will not be used
