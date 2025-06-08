947f64fb4f785f8405c821198a248aaea3d2c2b3
John Carlo Manuel <39042556+johncmanuel@users.noreply.github.com>
2025-06-08 10:11:08 -0700

    fix treesitter with xml parsing

---
+++ nvim/.config/nvim/lua/plugins/nvim-treesiter.lua
@@ -0,0 +1,7 @@
+return {
+  "nvim-treesitter/nvim-treesitter",
+  opts = function(_, opts)
+    opts.highlight = opts.highlight or {}
+    opts.highlight.disable = vim.list_extend(opts.highlight.disable or {}, { "xml" })
+  end,
+}
